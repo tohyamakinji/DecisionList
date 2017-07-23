f0f5e411f06db1457f42598bfd7b37029f7a35ec
tohyamakinji <putugitaandika@gmail.com>
2017-07-23 18:24:27 +0700

    Change variable name for more understanding

--- src/decisionlist/dataprocess/JSONWriter.java
+++ src/decisionlist/dataprocess/JSONWriter.java
@@ -21,30 +21,30 @@ public class JSONWriter {
 
     public void writeFeatures(JSONArray array) throws IOException, URISyntaxException {
         File file = new File(getClass().getResource("/result/featureresult.json").toURI());
-        OutputStream featureStream = new FileOutputStream(file, false);
-        featureStream.write(array.toJSONString().getBytes());
-        featureStream.flush();
-        featureStream.close();
+        OutputStream stream = new FileOutputStream(file, false);
+        stream.write(array.toJSONString().getBytes());
+        stream.flush();
+        stream.close();
         if (debugMode)
             System.out.println("DEBUG WRITE FILE - JSONWriter.java - writeFeatures() = SUCCESS");
     }
 
     public void writeUpdateTableDecision(JSONArray array) throws URISyntaxException, IOException {
         File file = new File(getClass().getResource("/result/decisiontable.json").toURI());
-        OutputStream featureStream = new FileOutputStream(file, false);
-        featureStream.write(array.toJSONString().getBytes());
-        featureStream.flush();
-        featureStream.close();
+        OutputStream stream = new FileOutputStream(file, false);
+        stream.write(array.toJSONString().getBytes());
+        stream.flush();
+        stream.close();
         if (debugMode)
             System.out.println("DEBUG WRITE FILE - JSONWriter.java - writeUpdateTableDecision() = SUCCESS");
     }
 
     public void writeTestingResult(JSONArray array) throws IOException, URISyntaxException {
         File file = new File(getClass().getResource("/result/testingresult.json").toURI());
-        OutputStream featureStream = new FileOutputStream(file, false);
-        featureStream.write(array.toJSONString().getBytes());
-        featureStream.flush();
-        featureStream.close();
+        OutputStream stream = new FileOutputStream(file, false);
+        stream.write(array.toJSONString().getBytes());
+        stream.flush();
+        stream.close();
         if (debugMode)
             System.out.println("DEBUG WRITE FILE - JSONWriter.java - writeTestingResult() = SUCCESS");
     }
